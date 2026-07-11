052c3645c4ffdd45e03d50589dd7dbe50dd4b45f
MSMITH71910 <m.smith71910@gmail.com>
2026-07-11 13:45:51 -0400

    Update build.sh paths and add runtime.txt for Render deployment

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 pip install -r requirements.txt
 
-cd mysite
-python manage.py collectstatic --no-input
-python manage.py migrate
+# Run migrations and collect static files
+# We use 'mysite/manage.py' because the project is in a subdirectory
+python mysite/manage.py collectstatic --no-input
+python mysite/manage.py migrate
